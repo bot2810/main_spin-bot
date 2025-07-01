4cfada9b9d649e6c9bd6e5e90f93661b83465d1f
bot2810 <removecity@gmail.com>
2025-07-01 09:05:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -313,7 +313,6 @@ def debug_tokens():
         'admin_id_configured': bool(ADMIN_ID)
     })
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
